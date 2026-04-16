211fa002fc6114b5d1194ed21adc932aa257333f
Darien Arthur-Gocken <233949874+DarienArthur-Gocken@users.noreply.github.com>
2026-04-16 11:35:14 -0700

    tree imgs

--- src/App.jsx
+++ src/App.jsx
@@ -17,14 +17,19 @@ function App() {
       </Section>
 
       <Section title="State Tree">
+        <img src="/western_hemlock.jpg" alt="A Western Hemlock tree standing proud with it's needled, evergreen form in the sunlight." />
         <Card title="Western Hemlock" description="Washington's official tree is the largest hemlock species, reaching 100-150 feet tall in moist coastal, lowland, and mountain forests. It has flat needles with rounded tips, tiny cones, thin bark with red inner wood, and a narrow, graceful crown." />
         <Card title="Why is it the state tree?" description="Designated in 1947, the western hemlock represents Washington's evergreen forests and timber legacy. Its broad range across damp forests and recognizable form made it a natural state symbol." />
       </Section>
 
       <Section title="Other trees in Washington">
+        <img src="/douglas_fir.jpg" alt="A Douglas Fir tree in the sunlight." />
         <Card title="Douglas Fir" description="Douglas fir has fast growth, soft blue-green needles, and reddish-brown bark. It thrives in many soils and can reach 200 feet or more in mature forests." />
+        <img src="/western_red_cedar.jpg" alt="A Western Red Cedar tree in a patch of other Western Red Cedars" />
         <Card title="Western Red Cedar" description="This tree has a wide base, and reddish bark. It prefers cool, moist summers, resists rot, and is used for shingles, siding, and traditional cultural carvings." />
+        <img src="/sitka_spruce.jpg" alt="A Sitka Spruce standing on it's lonesome." />
         <Card title="Sitka Spruce" description="Sitka spruce is a coastal conifer with blue-green needles and slightly drooping branches, typically found within about 50 miles of the Pacific. Its strong, lightweight wood is prized for aircraft, musical instruments, and boatbuilding." />
+        <img src="/ponderosa_pine.jpg" alt="A patch of Ponderosa Pines." />
         <Card title="Ponderosa Pine" description="A tall eastern Washington pine with orange-brown bark and needles usually in threes, Ponderosa thrives in dry, well-drained soils. It adapts to alkaline sites, wind, and high elevation, making it common on open slopes." />
       </Section>
 
