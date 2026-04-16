da0842261bcc3ede7eb93a48332cd7e58ec0cd8a
Darien Arthur-Gocken <233949874+DarienArthur-Gocken@users.noreply.github.com>
2026-04-16 10:56:15 -0700

    hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+function Hero() {
+  return (
+    <section className="hero">
+      <div>
+        <h2>Trees in Washington</h2>
+        <p>Washington State is host to a wide variety of trees due to it's proximity to the Pacific Ocean, and the wide range of climates and altitudes from sea level to tall mountaintops.</p>
+      </div>
+      <img src="/hero-image.jpg" alt="A tree identification book that tells how to depict common trees in Washington State" />
+    </section>
+  );
+}
