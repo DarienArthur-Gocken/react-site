e7ebd7d664724543c0489dc2263871cee8b9ece1
Darien Arthur-Gocken <233949874+DarienArthur-Gocken@users.noreply.github.com>
2026-04-16 13:24:00 -0700

    test fix images 2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
         <h2>Trees in Washington</h2>
         <p>This page discusses various trees in Washington, including the state tree and part of why Washington State is host to a wide variety of trees due to it's proximity to the Pacific Ocean, and the wide range of climates and altitudes from sea level to tall mountaintops.</p>
       </div>
-      <img src={`${baseUrl}hero-image.jpg`} alt="A tree identification book that tells how to depict common trees in Washington State" />
+      <img src={`${baseUrl}/public/hero-image.jpg`} alt="A tree identification book that tells how to depict common trees in Washington State" />
     </section>
   );
 }
