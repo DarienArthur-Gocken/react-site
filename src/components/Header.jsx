6197ba97068ea45fd3ab0cf2ed0c119fb3722bd2
Darien Arthur-Gocken <233949874+DarienArthur-Gocken@users.noreply.github.com>
2026-04-16 10:50:10 -0700

    basic header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1>Trees in Washington</h1>
+      <nav>
+        <a href="#overview">Overview</a>
+        <a href="#statetree">State Tree</a>
+        <a href="#douglasfir">Douglas Fir</a>
+        <a href="#westernredcedar">Western Red Cedar</a>
+      </nav>
+    </header>
+  );
+}
